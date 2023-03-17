7c17773bf6c627c65553524a33b5c489dab05ffc
Amirrulloh Azis <amirulloh.azis@gmail.com>
2023-03-17 11:08:59 +0700

    8.5_Create The Game Manager

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : SingletonMonobehaviour<GameManager>
+{
+    #region Header DUNGEON LEVELS
+    [Space(10)]
+    [Header("DUNGEON LEVELS")]
+    #endregion
+
+    #region Tooltip
+    [Tooltip("Populate with the dungeon level scriptable objects")]
+    #endregion
+
+    [SerializeField] private List<DungeonLevelSO> dungeonLevelList;
+
+    #region Tooltip
+    [Tooltip("Populate with the starting dungeon level for testing, first level = 0")]
+    #endregion
+
+    [SerializeField] private int currentDungeonLevelListIndex = 0;
+    [HideInInspector] public GameState gameState;
+
+    private void Start()
+    {
+        HandleGameState();
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            gameState = GameState.gameStarted;
+        }
+    }
+
+    private void HandleGameState()
+    {
+        switch (gameState)
+        {
+            case GameState.gameStarted:
+                //Play first level
+                PlayDungeonLevel(currentDungeonLevelListIndex);
+                gameState = GameState.playingLevel;
+                break;
+
+        }
+    }
+
+    private void PlayDungeonLevel(int dungeonLevelListIndex)
+    {
+
+    }
+
+    #region Validation
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(dungeonLevelList), dungeonLevelList);
+    }
+
+#endif
+    #endregion
+    
+}
