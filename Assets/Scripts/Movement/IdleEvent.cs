34aeba920b8b54168d76aae537cd1fc13a344c95
Amirrulloh Azis <amirulloh.azis@gmail.com>
2023-03-31 15:20:31 +0700

    10.2_Player Idle And Aim Weapon

---
+++ Assets/Scripts/Movement/IdleEvent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class IdleEvent : MonoBehaviour
+{
+    public event Action<IdleEvent> OnIdle;
+
+    public void CallIdleEvent()
+    {
+        OnIdle?.Invoke(this);
+    }
+}
