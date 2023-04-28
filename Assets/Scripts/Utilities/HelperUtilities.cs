0ea923e2bbef0ff2a9c710544813a6f200b7c257
Amirrulloh Azis <amirulloh.azis@gmail.com>
2023-04-28 14:51:01 +0700

    14.2_The Sound Effect Classes

--- Assets/Scripts/Utilities/HelperUtilities.cs
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -97,6 +97,17 @@ public static class HelperUtilities
 
     }
 
+    /// <summary>
+    /// Conver the linear volume scale to decibels
+    /// </summary>
+    public static float LinearToDecibels(int linear)
+    {
+        float linearScaleRange = 20f;
+
+        // formula to convert from the linear scale to the logarithmic decibel scale
+        return Mathf.Log10((float)linear / linearScaleRange) * 20f;
+    }
+
 
     /// <summary>
     /// Empty string debug check
